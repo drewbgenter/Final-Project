79f757c2773e5bfc3f17b399b2e292c9b350c2f5
drewbgenter <drewbgenter@gmail.com>
2014-05-31 13:17:44 -0500

    Associations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,5 @@
 class Project < ActiveRecord::Base
+  belongs_to :user
+  has_many :tasks
+  has_many :members, :through => :tasks, :source => :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :projects
+  has_many :tasks
+  has_many :claimed_tasks, :through => :tasks, :source => :project
 end
